4432b4883cd0b8c94ea3b060f0412928cd095c5d
JoseDavidPonce <60592092+JoseDavidPonce@users.noreply.github.com>
2020-02-16 17:25:51 -0600

    Código comentado

--- LCD_8bits_main.c
+++ LCD_8bits_main.c
@@ -1,4 +1,8 @@
 /* 
+ * 
+ * Autor: José David Ponce del Cid
+ * UVG
+ * Ingeniería electrónica
  * Se utilizó como referencia
  * la librería modificada por Pablo Mazariegos
  * originalmente escrita por Ligo George
@@ -7,35 +11,35 @@
 
 #include <pic16f887.h>
 #include <stdint.h>
-#include "LCD_8bits.h"
+#include "LCD_8bits.h"      //Se importan los headers correspondientes
 
-void LCD_CMD (uint8_t a){
-    RS = 0;
+void LCD_CMD (uint8_t a){   //Función para enviar comandos al puerto 
+    RS = 0;                 //de datos de la LCD
     PUERTO = a;
-    EN = 1;
-    __delay_us(5);
+    EN = 1;               //Después de enviar el comando, se activa el reloj
+    __delay_us(5);          //Para que tenga efecto
     EN = 0;
-    __delay_ms(5);
+    __delay_ms(5);          //Se espera por seguridad
 }
 
-void LCD_CLR (void){
+void LCD_CLR (void){        //Comando preestablecido para limpiar la LCD
     LCD_CMD(0x01);
 }
 
-void LCD_SET_CURSOR (uint8_t a, uint8_t b){
+void LCD_SET_CURSOR (uint8_t a, uint8_t b){ //Función para ubicar el cursor
     uint8_t temporal;
-    if (a == 1) {
-        temporal = 0x80 + b - 1;
-        LCD_CMD(temporal);
+    if (a == 1) {                   //a es la fila y b es la columna
+        temporal = 0x80 + b - 1;        //De esta manera el comando ya incluye      
+        LCD_CMD(temporal);      //la coordenada necesaria
     }
-    else if (a == 2){
-        temporal = 0xC0 + b - 1;
-        LCD_CMD(temporal);
+    else if (a == 2){           //En el caso de que se escoja la fila 2, se
+        temporal = 0xC0 + b - 1;    //hace lo mismo
+        LCD_CMD(temporal);      //Siempre se envía el comando
     }
 }
 
-void LCD_INIT (void) {
-    PUERTO = 0;
+void LCD_INIT (void) {          //Para inicializar la LCD, esta función envía
+    PUERTO = 0;         //la rutina necesaria para su inicialización
     __delay_ms(15);
     LCD_CMD(0x30);
     __delay_ms(5);
@@ -45,8 +49,8 @@ void LCD_INIT (void) {
     __delay_us(160);
     
     
-    LCD_CMD(0x02);
-    LCD_CMD(0x38);
+    LCD_CMD(0x02);      //Los siguientes comandos son de configuración cuando ya
+    LCD_CMD(0x38);      //inicializó la LCD
     LCD_CMD(0x08);
     LCD_CMD(0x01);
     LCD_CMD(0x06);
@@ -54,25 +58,25 @@ void LCD_INIT (void) {
 
 }
 
-void LCD_WRITE_CHAR(char a){
+void LCD_WRITE_CHAR(char a){    //Cuando en esta función entra una variable char
     RS = 1;
-    PUERTO = a;
-    EN = 1;
+    PUERTO = a;             //Se envía el dato al puerto de la LCD y se 
+    EN = 1;     //imprime con una señal de reloj
     __delay_us(40);
     EN = 0;       
     __delay_ms(5);
 }
 
-void LCD_WRITE_STRING (char *a){
-    int i;
-    for(i=0; a[i]!='\0'; i++)
+void LCD_WRITE_STRING (char *a){    //La función de STRING separa un arreglo
+    int i;                  //de chars y lo envía uno por uno con un ciclo for
+    for(i=0; a[i]!='\0'; i++)   //usando la función anterior
         LCD_WRITE_CHAR(a[i]);
 }
 
-void LCD_SHIFT_RIGHT(void){
-    LCD_CMD(0x1C);
+void LCD_SHIFT_RIGHT(void){ //Si se acabó el espacio de LCD, esta función mueve
+    LCD_CMD(0x1C);          //a la derecha todo el display
 }
 
-void LCD_SHIFT_LEFT(void){
-    LCD_CMD(0x18);
+void LCD_SHIFT_LEFT(void){  //Si se acabó el espacio de LCD, esta función mueve
+    LCD_CMD(0x18);          //a la izquierda todo el display
 }
--- main_lab3.c
+++ main_lab3.c
@@ -1,7 +1,8 @@
 /*
  * File:   main_lab3.c
- * Author: José P11
- *
+ * Author: José Ponce
+ * Ingeniería electrónica
+ * Carné 18187
  * Created on 9 de febrero de 2020, 07:50 PM
  */
 #pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
@@ -27,74 +28,73 @@
 #include "ADCheader.h"
 #include "EUSARTheader.h"
 
-#define _XTAL_FREQ 4000000
+#define _XTAL_FREQ 4000000  //Configura una frecuencia para los delays
 
-void Port_init(void);
+void Port_init(void);   //Prototipos de funciones
 uint8_t pot1, pot2, start_another;
-float volt1, volt2;
+float volt1, volt2;     //Variables float para los voltajes
 int valor = 0;
 char Contador;
-char string1[4];
+char string1[4];        //String para los valores de sprintf
 char string2[4];
 char string3[3];
 
-void __interrupt() isr(void){
-    if (PIR1bits.ADIF == 1){
+void __interrupt() isr(void){   //Módulo de interrupciones con ADC, y RX
+    if (PIR1bits.ADIF == 1){    //Si acabó una conversión ADC, apagar la bandera
         PIR1bits.ADIF = 0;
-        start_another = 1;
-        if (ADCON0bits.CHS0 == 0){
-            pot2 = ADRESH;
-            ADCON0bits.CHS0 = 1;
-        }else{
-            pot1 = ADRESH;
-            ADCON0bits.CHS0 = 0;
+        start_another = 1;      //e indicar con una bandera para comenzar otra
+        if (ADCON0bits.CHS0 == 0){  //Si el canal es el 0
+            pot2 = ADRESH;      //Guardar el valor en pot2
+            ADCON0bits.CHS0 = 1;    //Cambiar al canal 1
+        }else{         //Si el canal no es 0, es 1. Y se guarda el valor en 
+            pot1 = ADRESH;      //pot
+            ADCON0bits.CHS0 = 0;        //se regresa al canal 0
         }
-    }if (PIR1bits.RCIF == 1){
-        Contador = RCREG;
-        PIR1bits.RCIF = 0;
+    }if (PIR1bits.RCIF == 1){       //Si se acaba de recibir un dato
+        Contador = RCREG;           //hay que guardarlo en "contador"
+        PIR1bits.RCIF = 0;      //Apagar la badera de interrupción
     }
 }
 
-void main(void) {    
-    Port_init();
-    LCD_INIT();
-    ADC_init(1,1,1,0);
-    EUSART_Init(1, 0);
-    while(1){
-        if (start_another == 1){
-            start_conversion();
-            start_another = 0;
+void main(void) {       //Programa principal
+    Port_init();        //Configuración de los puertos
+    LCD_INIT();         //Inicialización de la LCD
+    ADC_init(1,1,1,0);      //Inicialización de la conversión ADC
+    EUSART_Init(1, 0);      //Inicialización de la comunicación serial y 9600 baudios
+    while(1){               //Comienzo del loop
+        if (start_another == 1){    //Si hay que iniciar otra conversión,
+            start_conversion();     //Activar la bandera go_done
+            start_another = 0;      //Y apagar el indicador
         }
-        if (Contador == 0x2B){
-            valor++;
-            Contador = 0;
-        }else if (Contador == 0x2D){
+        if (Contador == 0x2B){      //Si el contador es igual al signo +
+            valor++;        //Sumar 1 al valor de la variable de valor
+            Contador = 0;   //resetear variable contador para recibir otro dato
+        }else if (Contador == 0x2D){    //si el signo es -, hay que restar 1 al valor
             valor--;
             Contador = 0;
         }
-        valor = Contador;
-        volt1 = pot1*(5.0/255);
-        volt2 = pot2*(5.0/255);  
-        sprintf(string1,"%1.1f", volt1);
-        sprintf(string2, "%1.1f", volt2);
-        sprintf(string3, "%d", valor);
-        LCD_SET_CURSOR(1,1);
-        LCD_WRITE_STRING("V1   V2  Cont.");
-        LCD_SET_CURSOR(2,1);
-        LCD_WRITE_STRING(string1);
-        LCD_SET_CURSOR(2,6);
-        LCD_WRITE_STRING(string2);
+        volt1 = pot1*(5.0/255);     //Conversión de 0 - 255 a 0 - 5v 
+        volt2 = pot2*(5.0/255);         //Para visualizar los voltajes
+        sprintf(string1,"%1.1f", volt1);    //Se convierte el voltaje con 1 decimal
+        sprintf(string2, "%1.1f", volt2);   //en variables de string
+        sprintf(string3, "%d", valor);      //para ser enviadas con facilidad
+        LCD_SET_CURSOR(1,1);        //Se coloca el cursor en la primera fila
+        LCD_WRITE_STRING("V1   V2  Cont.");     //y primera columna
+        LCD_SET_CURSOR(2,1);        //Se coloca en la segunda fila para escribir 
+        LCD_WRITE_STRING(string1);      //los strings obtenidos
+        LCD_SET_CURSOR(2,6);        //Se corre para la siguiente string
+        LCD_WRITE_STRING(string2);      //Se imprime el segundo valor de voltaje
         LCD_SET_CURSOR(2, 11);
-        LCD_WRITE_STRING(string3);
-        TXREG = 0x45;
-        SEND_STRING(string1);
-        SEND_STRING(string2);
+        LCD_WRITE_STRING(string3);      //por último se imprime el contador
+        TXREG = 0x45;                   //Enviar un código de inicio de datos
+        SEND_STRING(string1);       //Para comenzar la recepción del otro lado
+        SEND_STRING(string2);       //de los tres valores, volt1, volt2 y contador
         SEND_STRING(string3);
     }
     return;
 }
 
-void Port_init(void){
+void Port_init(void){       //Inicialización de los puertos 
     TRISA = 0x03;
     ANSEL = 0;
     ANSEL = 0x03;
